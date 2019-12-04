3d32e06eb53145046b1c24a8371289321afa4116
danro9685 <daniele.ramazzotti@yahoo.com>
2019-12-04 22:39:56 +0100

    adding data and inference results

---
+++ R/data.R
@@ -0,0 +1,56 @@
+#' @name data_HN120Primary
+#' @title mutation data for cell line HN120 Primary from Sharma, A. et al. (2018).
+#' @description mutation data for cell line HN120 Primary from Sharma, A. et al. (2018).
+#' @docType data
+#' @usage data(data_HN120Primary)
+#' @format list of mutation data for 3 time points
+#' @source Sharma, A. et al. Longitudinal single-cell RNA sequencing of patient-derived primary cells reveals drug-induced infidelity in stem cell hierarchy. Nature communications 9, 4931 (2018).
+#' @return list of mutational data for cell line HN120 Primary
+NULL
+
+#' @name data_HN120Metastasis
+#' @title mutation data for cell line HN120 Metastasis from Sharma, A. et al. (2018).
+#' @description mutation data for cell line HN120 Metastasis from Sharma, A. et al. (2018).
+#' @docType data
+#' @usage data(data_HN120Metastasis)
+#' @format list of mutation data for 3 time points
+#' @source Sharma, A. et al. Longitudinal single-cell RNA sequencing of patient-derived primary cells reveals drug-induced infidelity in stem cell hierarchy. Nature communications 9, 4931 (2018).
+#' @return list of mutational data for cell line HN120 Metastasis
+NULL
+
+#' @name data_HN137Primary
+#' @title mutation data for cell line HN137 Primary from Sharma, A. et al. (2018).
+#' @description mutation data for cell line HN137 Primary from Sharma, A. et al. (2018).
+#' @docType data
+#' @usage data(data_HN137Primary)
+#' @format list of mutation data for 3 time points
+#' @source Sharma, A. et al. Longitudinal single-cell RNA sequencing of patient-derived primary cells reveals drug-induced infidelity in stem cell hierarchy. Nature communications 9, 4931 (2018).
+#' @return list of mutational data for cell line HN137 Primary
+NULL
+
+#' @name inference_HN120Primary
+#' @title results obtained with the function LACE on input data data_HN120Primary from Sharma, A. et al. (2018).
+#' @description results obtained with the function LACE on input data data_HN120Primary from Sharma, A. et al. (2018).
+#' @docType data
+#' @usage data(inference_HN120Primary)
+#' @format results obtained with the function LACE on input data data_HN120Primary
+#' @return results obtained with the function LACE on input data data_HN120Primary
+NULL
+
+#' @name inference_HN120Metastasis
+#' @title results obtained with the function LACE on input data data_HN120Metastasis from Sharma, A. et al. (2018).
+#' @description results obtained with the function LACE on input data data_HN120Metastasis from Sharma, A. et al. (2018).
+#' @docType data
+#' @usage data(inference_HN120Metastasis)
+#' @format results obtained with the function LACE on input data data_HN120Metastasis
+#' @return results obtained with the function LACE on input data data_HN120Metastasis
+NULL
+
+#' @name inference_HN137Primary
+#' @title results obtained with the function LACE on input data data_HN137Primary from Sharma, A. et al. (2018).
+#' @description results obtained with the function LACE on input data data_HN137Primary from Sharma, A. et al. (2018).
+#' @docType data
+#' @usage data(inference_HN137Primary)
+#' @format results obtained with the function LACE on input data data_HN137Primary
+#' @return results obtained with the function LACE on input data data_HN137Primary
+NULL
