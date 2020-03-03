da248707823be8037822b0d7957fdf897298f6fa
danro9685 <daniele.ramazzotti@yahoo.com>
2020-03-03 15:10:47 +0100

    handle indistinguishable inputs

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,40 @@
+# Remove any indistinguishable event from input data
+check.indistinguishable <- function( data ) {
+    
+    # build one data matrix from all time points
+    full_data <- NULL
+    for(i in 1:length(data)) {
+        full_data <- rbind(full_data,data[[i]])
+    }
+    
+    # check for indistinguishable events
+    indistinguishable <- as.numeric(which(duplicated(t(full_data))))
+    if(length(indistinguishable)>0) {
+        # merge names of indistinguishable events
+        valid_colnames <- colnames(full_data)[-indistinguishable]
+        new_colnames <- valid_colnames
+        invalid_colnames <- colnames(full_data)[indistinguishable]
+        for(i in invalid_colnames) {
+            for(j in valid_colnames) {
+                if(all(is.na(full_data[,i])==is.na(full_data[,j]))) { # if NAs in i and j are the same
+                    if(all(full_data[,i]==full_data[,j],na.rm=TRUE)) { # if not NAs entries in i and j are the same
+                        new_colnames[which(valid_colnames==j)] <- paste0(new_colnames[which(valid_colnames==j)],"|",invalid_colnames[which(invalid_colnames==i)])
+                        next
+                    }
+                }
+            }
+        }
+        # remove indistinguishable events from data
+        for(i in 1:length(data)) {
+            data[[i]] <- data[[i]][,valid_colnames,drop=FALSE]
+            colnames(data[[i]]) <- new_colnames
+        }
+    }
+    
+    return(data)
+    
+}
+
 # Build adjacency matrix from B as returned from the function learn.longitudinal.phylogeny
 as.adj.matrix <- function( B ) {
     
