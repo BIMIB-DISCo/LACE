0a190476006178d52b0f606289b3f9cbe44e8823
danro9685 <daniele.ramazzotti@yahoo.com>
2020-07-26 19:35:48 +0200

    empty gh-pages branch

--- tests/testthat.R
+++
@@ -1,6 +0,0 @@
-Sys.setenv("R_TESTS" = "")
-
-library("testthat")
-library("LACE")
-
-test_check("LACE")
--- tests/testthat/test_algorithm.R
+++
@@ -1,8 +0,0 @@
-data("longitudinal_sc_variants")
-
-context("LACE")
-
-data("inference")
-test_that("LACE produces correct output", {
-	expect_equal(names(inference),c("B","C","corrected_genotypes","clones_prevalence","relative_likelihoods","joint_likelihood","clones_summary","equivalent_solutions","error_rates"))
-})
